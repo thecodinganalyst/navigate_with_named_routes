c8bbbdd2b2c6ec0324e22cfc09333d1eab3b4371
Dennis Cai <dennis.cai@zuhlke.com>
2022-10-31 18:23:29 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/navigate_with_named_routes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navigate_with_named_routes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Named Routes Demo',
+      initialRoute: '/', // When using initialRoute, don't define a home property
+      routes: {
+        '/': (context) => const FirstScreen(),
+        '/second': (context) => const SecondScreen()
+      },
+    )
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Launch screen'),
+        )
+      )
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go Back!'),
+        ),
+      ),
+    );
+  }
+}
+
